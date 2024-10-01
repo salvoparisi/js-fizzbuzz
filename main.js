9d1c0c6f2801e5153a40b138ea9db30acfe22028
salvoparisi <psalvo065@gmail.com>
2024-10-01 20:38:13 +0200

    add FizzBuzz

--- main.js
+++ main.js
@@ -2,13 +2,18 @@
 for (let i=1; i<=100; i++){
     //dividiamo la i per 3, se il resto è uguale a 0 vuol dire che è un multiplo 
     if(i % 3 == 0){
-        //stampiamo Fizz
-        console.log("Fizz");
+        if(i % 5 == 0){    //se sara pure multiplo di 5 scrivera "FizzBuzz"
+            console.log("FizzBuzz");
+        }else{            
+            console.log("Fizz");
+        }
     }else if (i % 5 == 0){      //stessa cosa per Buzz ma con i multipli di 5
-        console.log("Buzz");
-    }else{
-        //se la i non è ne multiplo di 3 ne multiplo di 5, stampa direttamente il numero
-        console.log(i);
-        
+        if(i % 3 == 0){
+            console.log("FizzBuzz")
+        }else{
+            console.log("Buzz");
+        }
+    }else{          //se non sarà un muliplo di 3 o 5 scriverà semplicemente un numero
+        console.log(i)
     }
 }
